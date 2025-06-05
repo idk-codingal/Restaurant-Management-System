051cf39aaf5752e57929975c695a85623adfd036
idk-codingal <mokshnagda2020@gmail.com>
2025-06-04 17:23:51 -0700

    hi

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class Restaurantapp:# Class
         root.title("Restaurant App")
         # menu
         self.menu = {
-            "Non-Veg Options": 0.00, 
+            "Non-Veg Options": None, 
             "cheese burger": 2.99,
             "Steak": 5.99,
             "Fries": 2.50,
             "Pizza": 7.99,
-            "Veg Options": {},
+            "Veg Options": None,
             "Veg Pizza": 7.99,
             "Veg Burger": 3.99,
             "Sodas": 0.00,
